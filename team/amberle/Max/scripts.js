901f69a54ff2bdd44bf6fc64901d6c0820612308
MaxEWatson <45746782+MaxWato@users.noreply.github.com>
2025-03-03 13:47:35 +1100

    funny update

---
+++ team/amberle/Max/scripts.js
@@ -0,0 +1,5 @@
+let ButtonClick = document.getElementById('ButtonRun');
+
+ButtonClick.addEventListener('click', function () {
+    window.open('https://youtu.be/dQw4w9WgXcQ?si=-Yd5m8YSJMTgt6db', '_black');
+})
